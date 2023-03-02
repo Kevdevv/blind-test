4e019b310b2b8e305af2924f162272e117789a2e
Kevin Rama <ramanantsoa.vlm@gmail.com>
2023-03-02 17:29:19 +0100

    add timer

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Test from '../sound/test.mp3'
 import Sound from '../component/Sound'
 import Eye from '../sound/21.mp3'
@@ -8,6 +8,21 @@ const Landing = () => {
   const [currentComponent, setCurrentComponent] = useState(1);
   const totalComponents = 3;
 
+  const [seconds, setSeconds] = useState(3);
+
+  useEffect(() => {
+
+    if (seconds === 0) {
+      setCurrentComponent(currentComponent +1)
+      return
+    }
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
+
 
   function change() {
     setCurrentComponent(currentComponent < totalComponents ? currentComponent + 1 : 1);
@@ -16,7 +31,9 @@ const Landing = () => {
 
   return (
     <>
-
+    <div>
+      <h1>Timer: {seconds} secondes</h1>
+    </div>
     <button onClick={change}>autre son</button>
     {(() => {
         switch (currentComponent) {
